9fe267986b42513054ecf2cc51a16a207579f935
James <jamescwylde@gmail.com>
2022-02-06 16:41:21 +0000

    Revamp

--- Install.ps1
+++ Install.ps1
@@ -4,12 +4,13 @@ $sep = "____________________________________________________________"
 
 
 
-
+Write-Host "James Wylde - https://github.com/jameswylde/powershell-profile-setup.git" -ForegroundColor White -BackgroundColor Blue
 
 # Install required fonts
 
 Write-Host $sep -ForegroundColor Green
-Write-Host "Installing required Nerd fonts" -ForegroundColor Green
+Write-Host "Installing required Nerd fonts - [1/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 try{
     $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
     foreach ($file in Get-ChildItem *.ttf)
@@ -23,30 +24,33 @@ try{
     #Copy-Item *.ttf c:\windows\fonts\
     }
 catch{[EXCEPTION]}
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
 
 # Update JSON with env:Username added to paths
 
-Write-Host "Setting user specific json variales" -ForegroundColor Green
+Write-Host "Setting user specific json variales - [2/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     $json = Get-Content "settings.json" | ConvertFrom-Json 
     $json.profiles.list[0].backgroundImage = "C:\Users\$env:Username\Documents\PowerShell\WT_images\win.png"
     $json | ConvertTo-Json -Depth 9 | Out-File "settings.json"
 
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
 # Set PSGallery as trusted (confirm flag for Install-Module still prompt on untrusted repo
 
-Write-Host "Setting PSGallery as trusted repo (revert if needed)" -ForegroundColor Green
+Write-Host "Setting PSGallery as trusted repo - [3/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 
 # Oh-My-Posh install, add to default prompt, add theme
 
-Write-Host "Installing Oh-MyPosh" -ForegroundColor Green
+Write-Host "Installing Oh-MyPosh - [4/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     $input = Read-Host "Install Oh-My-Posh? [y/n]" 
     switch($input){
@@ -59,14 +63,15 @@ Write-Host "Installing Oh-MyPosh" -ForegroundColor Green
         default{write-warning "Y or N only."}
     }
 
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
 
 
 # Setting OMp as prompt and adding theme
     
-Write-Host "Setting Oh-My-Posh as prompt and setting theme" -ForegroundColor Green
+Write-Host "Setting Oh-My-Posh as prompt and setting theme - [5/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     try{
         $dest = "C:\Users\$env:Username\AppData\Local\Programs\oh-my-posh\themes"
@@ -76,33 +81,34 @@ Write-Host "Setting Oh-My-Posh as prompt and setting theme" -ForegroundColor Gre
         }
     catch{[EXCEPTION]}
 
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
 
 
 # Module installation
 
-Write-Host "Installing Z,PsReadLine,Terminal-Icons" -ForegroundColor Green
+Write-Host "Installing Z,PsReadLine,Terminal-Icons modules - [6/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     try{
         Install-Module -Name z -RequiredVersion 1.1.3 -confirm:$false
         Install-Module -Name Terminal-Icons -RequiredVersion 0.8.0 -confirm:$false
-        Get-InstalledModule -Name psreadline -AllVersions | Uninstall-Module
+        #Get-InstalledModule -Name psreadline -AllVersions | Uninstall-Module
         Install-Module PSReadLine -Force -AllowPrerelease -SkipPublisherCheck
         # Install-Module -Name PSReadLine -RequiredVersion 2.2.0-beta5 -AllowPrerelease -confirm$false
         }
     catch{Write-Output $_.Exception.GetType().Name}
 
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
 
 
 
 # Set PowerShell profile to incorporate OMP and Module settings 
-Write-Host "Setting Powershell profile" -ForegroundColor Green
-Write-Host ""
+Write-Host "Setting Powershell profile - [7/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     try{
         $dest2 = "C:\Users\$env:Username\Documents\PowerShell"
@@ -113,15 +119,16 @@ Write-Host ""
     catch{[EXCEPTION]}
 
 Write-Host "NOTE - If you are using OneDrive, you may need to copy into OneDrive\Documents\Powershell" -ForegroundColor Red -BackgroundColor Yellow
-Write-Host ""
-Write-Host $sep -ForegroundColor Green
+
+
 Write-Host ""
 
 
 
 # Windows Terminal setup
 
-Write-Host "Windows Terminal default settings" -ForegroundColor Green
+Write-Host "Windows Terminal default settings - [8/8]" -ForegroundColor Green
+Write-Host $sep -ForegroundColor Green
 
     try{
         $dest3 = "C:\Users\$env:Username\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"
@@ -135,11 +142,11 @@ Write-Host "Windows Terminal default settings" -ForegroundColor Green
         $dest4 = "C:\Users\$env:Username\Documents\PowerShell\WT_images\"
 
         if (!(Test-Path -path $dest4)) {New-Item $dest4 -Type Directory}
-        Copy-Item "win.png","ubu.png" -Destination $dest4
+        Copy-Item "win.png" -Destination $dest4
         }
     catch{[EXCEPTION]}
 
 
-Write-Host $sep -ForegroundColor Green
+
 Write-Host ""
 
